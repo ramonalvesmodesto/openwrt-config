b0410993659d5e150edcd7ce47465470f2da89b5
Ramon Alves <60831086+ramonalvesmodesto@users.noreply.github.com>
2024-09-06 23:47:57 -0300

    Update config_ipv6_with_prefix_delegate.sh

--- ipv6/config_ipv6_with_prefix_delegate.sh
+++ ipv6/config_ipv6_with_prefix_delegate.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
+random_ipv6=$(openssl rand -hex 8)
+random_1=${random_ipv6:0:4}
+random_2=${random_ipv6:4:4}
+random_3=${random_ipv6:8:4}
+random_4=${random_ipv6:12:4}
+
 rm /var/dhcpv6.leases
 touch /var/dhcpv6.leases
-dhclient -6 -cf /etc/dhclient6.conf  -sf /usr/sbin/dhclient-script -lf /var/dhcpv6.leases eth0 #Usa o isc-dhcpv6-client para obter o ipv6 do isp
+dhclient -6 -cf /etc/dhclient6.conf  -sf /usr/sbin/dhclient-script -lf /var/dhcpv6.leases eth0
 
 IPV6_128="$(cat /var/dhcpv6.leases | grep iaaddr | cut -d ' ' -f 6- | cut -d '{' -f 1 | cut -d ' ' -f 1)/128"
 IPV6_SUFIX="$(cat /var/dhcpv6.leases | grep iaaddr | cut -d ' ' -f 6- | cut -d '{' -f 1 | sed 's/::/\//' | cut -d '/' -f 1)"
-EU64_MAC="1C1A:EAFF:FE28:1CEC"
-IPV6_64="$IPV6_SUFIX:$EU64_MAC/64"
+IPV6_RANDOM="$random_1:$random_2:$random_3:$random_4"
+IPV6_64="$IPV6_SUFIX:$IPV6_RANDOM/64"
 
 uci del dhcp.wan6
 uci del network.wan6
@@ -34,13 +40,6 @@ uci set dhcp.wan6.interface='wan6'
 uci set dhcp.wan6.ignore='1'
 uci set dhcp.wan6.ra='relay'
 uci set dhcp.wan6.dhcpv6='relay'
-
-uci set firewall.@zone[1].masq="1"
-uci set firewall.@zone[1].masq6="1"
-uci commit firewall
-service firewall restart
-
-uci set network.wan6.sourcefilter="0"
 
 uci commit
 /etc/init.d/network restart
