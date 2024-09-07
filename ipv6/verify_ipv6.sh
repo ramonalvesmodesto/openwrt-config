aa258658bccbae9f296395b14b1b30b1d9277998
Ramon Alves <60831086+ramonalvesmodesto@users.noreply.github.com>
2024-09-07 13:44:42 -0300

    Update verify_ipv6.sh

--- ipv6/verify_ipv6.sh
+++ ipv6/verify_ipv6.sh
@@ -5,9 +5,9 @@ status_code=$(curl -I ip6only.me 2>/dev/null | head -n 1 | cut -d$' ' -f2)
 
 if [ "$status_code" == "" ]
 then
-        logger 'Status ipv6 Down'
+        logger 'Status ipv6: Down'
         /bin/sh /etc/config_ipv6_with_prefix_delegate.sh
 elif [ "$status_code" -eq "200" ]
 then
-        logger 'Status ipv6 ok'
+        logger 'Status ipv6: Ok'
 fi
