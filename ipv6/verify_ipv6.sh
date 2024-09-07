fecc92c76d50894d95816ea97d9eff890a1f907d
Ramon Alves <60831086+ramonalvesmodesto@users.noreply.github.com>
2024-09-07 10:13:21 -0300

    Update verify_ipv6.sh

--- ipv6/verify_ipv6.sh
+++ ipv6/verify_ipv6.sh
@@ -1,11 +1,13 @@
+  GNU nano 8.1                                                                            verify_ipv6.sh                                                                                         
 #!/bin/sh
 
 status_code=$(curl -I ip6only.me 2>/dev/null | head -n 1 | cut -d$' ' -f2)
 
 if [ "$status_code" == "" ]
 then
+        logger 'Status ipv6 Down'
         /bin/sh /etc/config_ipv6_with_prefix_delegate.sh
 elif [ "$status_code" -eq "200" ]
 then
-	echo 'Status ok'
+        logger 'Status ipv6 ok'
 fi
