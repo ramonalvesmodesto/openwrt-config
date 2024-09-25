9d941933034325ae47b9b5adc9deb7d046ba11f4
Ramon Alves <60831086+ramonalvesmodesto@users.noreply.github.com>
2024-09-25 16:14:09 -0300

    Create README.mk

---
+++ css/README.mk
@@ -0,0 +1,4 @@
+```bash
+cp /www/luci-static/bootstrap/cascade.css /www/luci-static/bootstrap/cascade.css.bk
+wget https://raw.githubusercontent.com/ramonalvesmodesto/openwrt-config/refs/heads/main/css/cascade.css -O /www/luci-static/bootstrap/cascade.css
+```
