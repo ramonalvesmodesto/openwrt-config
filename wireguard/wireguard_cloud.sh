21ca30d88af995fc3bb5af377545a8d39c1f7814
Ramon Alves <60831086+ramonalvesmodesto@users.noreply.github.com>
2025-10-12 12:15:32 -0300

    Add files via upload

---
+++ wireguard/wireguard_cloud.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIA=$(date | cut -d' ' -f3)
+NUMBER=1
+
+if [ $DIA -gt 0 ] && [ $DIA -le 20 ]; then
+    NUMBER=$DIA
+elif [ $DIA -gt 21 ] && [ $DIA -le 30 ]; then
+    A=$(echo "$DIA" | grep -o .)
+    NUMBER=$(echo "$A" | tail -n 1)
+else
+    NUMBER=20
+fi
+
+IP=162.159.192.$NUMBER
+
+uci set network.@wireguard_cloud[0].endpoint_host="$IP"
+uci commit
+service network restart
