b874c0f04e4a1d821e2685ff9275375376f149c9
Ramon Alves <60831086+ramonalvesmodesto@users.noreply.github.com>
2025-10-12 12:49:27 -0300

    Update wireguard_cloud.sh

--- wireguard/wireguard_cloud.sh
+++ wireguard/wireguard_cloud.sh
@@ -4,12 +4,10 @@ DIA=$(date | cut -d' ' -f3)
 NUMBER=1
 NUMBER1=$(uci get network.@wireguard_cloud[0].endpoint_host | cut -d'.' -f4)
 
-if [ $NUMBER -eq $NUMBER1 ]; then
+if [ $DIA -eq $NUMBER1 ]; then
     logger 'Script exit wireguard_cloud.sh'
     exit 0
-fi
-
-if [ $DIA -gt 0 ] && [ $DIA -le 20 ]; then
+elif [ $DIA -gt 0 ] && [ $DIA -le 20 ]; then
     NUMBER=$DIA
 elif [ $DIA -gt 21 ] && [ $DIA -le 29 ]; then
     A=$(echo "$DIA" | grep -o .)
