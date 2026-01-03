0466e0e069949454b3f0637d57a01ef59a0b26d6
Ramon Alves <60831086+ramonalvesmodesto@users.noreply.github.com>
2026-01-03 19:56:08 -0300

    Update wireguard_cloud.sh

--- wireguard/wireguard_cloud.sh
+++ wireguard/wireguard_cloud.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-DIA=$(date | cut -d' ' -f3)
-NUMBER=1
-NUMBER1=$(uci get network.@wireguard_cloud[0].endpoint_host | cut -d'.' -f4)
+#DIA=$(date | cut -d' ' -f3)
+#NUMBER=1
+#NUMBER1=$(uci get network.@wireguard_cloud[0].endpoint_host | cut -d'.' -f4)
+NUMBERRAMDOM=$(echo $((RANDOM % 254)))
 
-if [ $DIA -eq $NUMBER1 ]; then
-    logger 'Script exit wireguard_cloud.sh'
-    exit 0
-elif [ $DIA -ge 4 ] && [ $DIA -le 20 ]; then
-    NUMBER=$DIA
-elif [ $DIA -ge 25 ] && [ $DIA -le 29 ]; then
-    A=$(echo "$DIA" | grep -o .)
-    NUMBER=$(echo "$A" | tail -n 1)
-else
-    NUMBER=16
-fi
+#if [ $DIA -eq $NUMBER1 ]; then
+#    logger 'Script exit wireguard_cloud.sh'
+#    exit 0
+#elif [ $DIA -ge 4 ] && [ $DIA -le 20 ]; then
+#    NUMBER=$DIA
+#elif [ $DIA -ge 25 ] && [ $DIA -le 29 ]; then
+#    A=$(echo "$DIA" | grep -o .)
+#    NUMBER=$(echo "$A" | tail -n 1)
+#else
+#    NUMBER=16
+#fi
 
-IP=162.159.192.$NUMBER
-uci set network.@wireguard_cloud[0].endpoint_host="$IP"
+#IP=162.159.192.$NUMBER
+IPCGNAT=172.16.1.$NUMBERRAMDOM/32
+uci set network.cloud.addresses="$IPCGNAT"
+#uci set network.@wireguard_cloud[0].endpoint_host="$IP"
 uci commit
 service network restart
