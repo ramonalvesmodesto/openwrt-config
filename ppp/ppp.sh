a8cf97d00748b71b95cd081af221eac9e74b5dc6
Ramon Alves <60831086+ramonalvesmodesto@users.noreply.github.com>
2026-01-11 18:26:22 -0300

    Update ppp.sh

--- ppp/ppp.sh
+++ ppp/ppp.sh
@@ -159,6 +159,10 @@ ppp_generic_setup() {
                 ${ipv6:+ipv6-up-script /lib/netifd/ppp6-up} \
                 ip-down-script /lib/netifd/ppp-down \
                 ${ipv6:+ipv6-down-script /lib/netifd/ppp-down} \
+                bsdcomp 0 \
+                deflate 0 \
+                novj \
+                novjccomp \
                 "$@" $pppd_options
 }
 
