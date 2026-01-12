f0f6846ad2de5ce3b836745824916f2d4580bfb2
Ramon Alves <60831086+ramonalvesmodesto@users.noreply.github.com>
2026-01-12 13:21:07 -0300

    Update wireguard_cloud.sh

--- wireguard/wireguard_cloud.sh
+++ wireguard/wireguard_cloud.sh
@@ -4,21 +4,26 @@ sleep 10
 NUMBERRAMDOM=$(echo $((RANDOM % 254)))
 NUMBERRAMDOMOTHER=$(echo $((RANDOM % 254)))
 NUMBERRAMDOMSIXTEEN=$(echo $((RANDOM % (127 - 16 + 1)+  16)))
-NUMBERRAMDOMDOMAIN=$(echo $((RANDOM % 4)))
+#NUMBERRAMDOMDOMAIN=$(echo $((RANDOM % 4)))
 NUMBERRAMDOMPORT=$(echo $((RANDOM % (5999 - 5000 + 1) + 5000)))
 
-list=("server_names = ['quad9-dnscrypt-ip4-filter-pri']"
-        "server_names = ['adguard-dns-unfiltered']"
-        "server_names = ['dnscry.pt-valdivia-ipv4']"
-        "server_names = ['google']"
-        "server_names = ['cloudflare']"
-     )
+#list=("server_names = ['quad9-dnscrypt-ip4-filter-pri']"
+#        "server_names = ['adguard-dns-unfiltered']"
+#        "server_names = ['dnscry.pt-valdivia-ipv4']"
+#        "server_names = ['google']"
+#        "server_names = ['cloudflare']"
+#     )
 
-sed -i "32 s/.*/${list[$NUMBERRAMDOMDOMAIN]}/" /etc/dnscrypt-proxy2/*.toml
+#sed -i "32 s/.*/${list[$NUMBERRAMDOMDOMAIN]}/" /etc/dnscrypt-proxy2/*.toml
+
+
+
+
+
 
 IPCGNAT=172.$NUMBERRAMDOMSIXTEEN.$NUMBERRAMDOMOTHER.$NUMBERRAMDOM/32
 uci set network.cloud.addresses="$IPCGNAT"
 uci set network.cloud.listen_port="$NUMBERRAMDOMPORT"
 uci commit
-service dnscrypt-proxy restart
+#service dnscrypt-proxy restart
 service network restart
