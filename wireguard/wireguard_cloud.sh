9e647e9b2fa264816cc8e45b8c1f2f3f21158196
Ramon Alves Modesto <60831086+ramonalvesmodesto@users.noreply.github.com>
2026-01-27 23:02:52 -0300

    Update wireguard_cloud.sh

--- wireguard/wireguard_cloud.sh
+++ wireguard/wireguard_cloud.sh
@@ -4,17 +4,8 @@ sleep 10
 NUMBERRAMDOM=$(echo $((RANDOM % 254)))
 NUMBERRAMDOMOTHER=$(echo $((RANDOM % 254)))
 NUMBERRAMDOMSIXTEEN=$(echo $((RANDOM % (127 - 16 + 1)+  16)))
-#NUMBERRAMDOMDOMAIN=$(echo $((RANDOM % 4)))
 NUMBERRAMDOMPORT=$(echo $((RANDOM % (5999 - 5000 + 1) + 5000)))
 
-#list=("server_names = ['quad9-dnscrypt-ip4-filter-pri']"
-#        "server_names = ['adguard-dns-unfiltered']"
-#        "server_names = ['dnscry.pt-valdivia-ipv4']"
-#        "server_names = ['google']"
-#        "server_names = ['cloudflare']"
-#     )
-
-#sed -i "32 s/.*/${list[$NUMBERRAMDOMDOMAIN]}/" /etc/dnscrypt-proxy2/*.toml
 
 
 
@@ -24,8 +15,6 @@ NUMBERRAMDOMPORT=$(echo $((RANDOM % (5999 - 5000 + 1) + 5000)))
 IPCGNAT=172.$NUMBERRAMDOMSIXTEEN.$NUMBERRAMDOMOTHER.$NUMBERRAMDOM/32
 IPV6=2606:4700:110:8c2e:ab97:d031:8048:fe2d/128
 uci set network.cloud.addresses="$IPCGNAT $IPV6"
-#uci set network.cloud.listen_port="$NUMBERRAMDOMPORT"
-#uci set firewall.@nat[0].snat_ip="$IPCGNAT"
 uci commit
 service firewall restart
 service network restart
