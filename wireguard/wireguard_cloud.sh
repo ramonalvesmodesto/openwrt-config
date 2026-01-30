068395971816983d69f6fb5fecf8885e8e2e2cad
Ramon Alves Modesto <60831086+ramonalvesmodesto@users.noreply.github.com>
2026-01-30 02:23:59 -0300

    Update

--- wireguard/wireguard_cloud.sh
+++ wireguard/wireguard_cloud.sh
@@ -13,7 +13,6 @@ NUMBERRAMDOMPORT=$(echo $((RANDOM % (5999 - 5000 + 1) + 5000)))
 
 
 IPCGNAT=172.$NUMBERRAMDOMSIXTEEN.$NUMBERRAMDOMOTHER.$NUMBERRAMDOM/32
-IPV6=2606:4700:110:8c2e:ab97:d031:8048:fe2d/128
 uci add_list network.cloud.ip6prefix='2606:4700:110:8c2e:ab97:d031:8048:fe2d/64'
 uci set network.cloud.addresses="$IPCGNAT $IPV6"
 uci commit
