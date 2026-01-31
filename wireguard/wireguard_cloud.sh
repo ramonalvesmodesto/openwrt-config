e2f552a49a27c7cf4ab6a996804d6f3b51019d45
Ramon Alves Modesto <60831086+ramonalvesmodesto@users.noreply.github.com>
2026-01-31 19:03:54 -0300

    Update wireguard_cloud.sh

--- wireguard/wireguard_cloud.sh
+++ wireguard/wireguard_cloud.sh
@@ -25,7 +25,7 @@ IPCGNAT=172.$NUMBERRAMDOMSIXTEEN.$NUMBERRAMDOMOTHER.$NUMBERRAMDOM/32
 IPCGNATISP=$(ip -4 a show pppoe-wan | grep inet | cut -d' ' -f6)/32
 IPV6=2606:4700:110:8c2e:ab97:d031:8048:fe2d/128
 
-uci set firewall.@zone[1].masq_src="$IPCGNAT"
+#uci set firewall.@zone[1].masq_src="$IPCGNAT"
 #uci set firewall.@zone[1].masq_dest="$IPCGNATISP"
 uci set network.cloud.addresses="$IPCGNAT $IPV6"
 #uci set network.cloud.listen_port="$NUMBERRAMDOMPORT"
