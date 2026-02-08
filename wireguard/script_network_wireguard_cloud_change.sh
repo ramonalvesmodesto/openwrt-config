c5299d9b797d749a105bdd1ba18400f282a73434
Ramon Alves Modesto <60831086+ramonalvesmodesto@users.noreply.github.com>
2026-02-08 12:21:46 -0300

    Update script_network_wireguard_cloud_change.sh

--- wireguard/script_network_wireguard_cloud_change.sh
+++ wireguard/script_network_wireguard_cloud_change.sh
@@ -2,6 +2,3 @@
 VAR=$1
 wget https://raw.githubusercontent.com/ramonalvesmodesto/openwrt-config/refs/heads/main/firewall/firewall$VAR.conf -O /etc/config/firewall
 wget https://raw.githubusercontent.com/ramonalvesmodesto/openwrt-config/refs/heads/main/network/network$VAR -O /etc/config/network
-
-service network restart
-
